dc487a2759f2c1765ffa54d1c50d4a40b8450073
Nikita88888 <74507770+Nikita88888@users.noreply.github.com>
2021-08-23 23:10:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//Buttons
+
+document.getElementById('youtube').onclick = function () {
+
+    console.log("YouTube")
+}
+
+document.getElementById('instagram').onclick = function () {
+
+    console.log("Instagram")
+}
+
+document.getElementById('tiktok').onclick = function () {
+
+    console.log("tiktok")
+}
+
+document.getElementById('youtube_2').onclick = function () {
+
+    console.log("YouTube 2")
+}
+
+
+//Display Größe
+
+//-----Es muss noch ein weg gefunden werden-----
+
+/*if (screen.width <= 285) {
+    console.log(":(")
+    document.getElementById('style').href = 'assets/css/285.css';
+}
+else if (screen.width <= 494) {
+    console.log(":|")
+    document.getElementById('style').href = 'assets/css/494.css';
+}
+else if (screen.width >= 495) {
+    console.log(":)")
+}*/
